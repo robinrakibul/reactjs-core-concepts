3bf4a0ca102dfca8c131b0ed44b62ef7837fd6ed
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-15 22:43:53 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 55555;
+const singer = {name: 'Jaden', job:'singing'};
+
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white'
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>Hello Dude, React. How are you?</h3>  
+        </div>
+        <div className="music">
+          <p style={{color:'blue',backgroundColor:'yellow'}}>Name: {number+2222}</p>
+          <p style={singerStyle}>Name: {singer.name}, Job: {singer.job}</p>  
+        </div>
+        <div className='App'>
+        <Person></Person>
+        <Friend></Friend>
+        </div>
       </header>
     </div>
   );
+}
+
+function Person(){
+  return <h1>Returning</h1>
+}
+
+function Friend(){
+  return (
+    <div className='container'>
+      <h3>Name: Jackey</h3>
+      <p>Job: Fighting</p>
+    </div>
+  )
 }
 
 export default App;
